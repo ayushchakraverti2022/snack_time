4279412e5e7a1a8d7901c8a9fcfdeba0f1505480
Your Name <you@example.com>
2024-08-09 23:28:02 +0530

    hh

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
-import 'package:snack_time/admin/add_food.dart';
-import 'package:snack_time/admin/admin_login.dart';
-import 'package:snack_time/admin/home_admin.dart';
-import 'package:snack_time/cubits/database/database_cubit.dart';
-import 'package:snack_time/cubits/logincubit/login_cubit.dart';
+import 'package:snack_time/cubits/cart/cart_cubit.dart';
+import 'package:snack_time/cubits/login/login_cubit.dart';
 import 'package:snack_time/cubits/sign/sign_up_cubit.dart';
 import 'package:snack_time/firebase_options.dart';
 import 'package:snack_time/page/botttom_navigation.dart';
-import 'package:snack_time/page/login.dart';
 import 'package:snack_time/page/onboard.dart';
-import 'package:snack_time/page/order.dart';
-import 'package:snack_time/page/profile.dart';
-import 'package:snack_time/page/signup.dart';
-import 'package:snack_time/page/wallet.dart';
 import 'package:snack_time/widget/app_constant.dart';
 
 Future<void> main() async {
@@ -40,7 +31,7 @@ class MyApp extends StatelessWidget {
           create: (context) => LoginCubit(),
         ),
         BlocProvider(
-          create: (context) => DatabaseCubit(),
+          create: (context) => CartCubit(),
         ),
       ],
       child: GetMaterialApp(
@@ -53,5 +44,8 @@ class MyApp extends StatelessWidget {
           )),
     );
   }
+}
+
+class DatabaseCubit {
 }
 
